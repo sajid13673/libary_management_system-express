02f4aa7873140da38cdb34d01a01a53d2d3eed14
Sajid Ahamed <sajid13673@gmail.com>
2025-03-11 16:17:10 +0530

    feat: Add hasPendingFine attribute in getMemberById borrowings and fix response handling
    - Added hasPendingFine attribute to member.borrowings in the getMemberById API for fine status
    - Resolved issue of multiple responses in deleteBorrowing by ensuring proper return statements
    - Formatted code for improved readability and consistency

--- controllers/borrowingController.js
+++ controllers/borrowingController.js
@@ -1,106 +1,135 @@
-const {Borrowing, Member, Book, Fine} = require('../models');
+const { Borrowing, Member, Book, Fine } = require("../models");
 
 const getBorrowings = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-          const perPage = parseInt(req.query.perPage) || 10;
-          const type = req.query.type || 'all';
-            const queryOptions  = {
-              offset: (page - 1) * perPage,
-              limit: perPage,
-              include: [
-                {
-                model: Book,
-                as: "book"
-              },
-              {
-                model: Member,
-                as: "member"
-              },
-              {
-                model: Fine,
-                as: "fine"
-              }
-            ]
-            };
-            if(type !== 'all'){
-              queryOptions.where = {status: type === 'returned' ? false : true}
-            }
-            const totalItems = await Borrowing.count(queryOptions);
-            const totalPages = Math.ceil(totalItems / perPage);
-            const borrowings = await Borrowing.findAll(queryOptions);
-            const borrowingsWithFineStatus = borrowings.map((borrowing) => {
-              const hasPendingFine = borrowing.fine ? !borrowing.fine.isPaid : false;
-              const { borrowings, ...borrowingWithoutFines } = borrowing.toJSON();
-              return {
-                ...borrowingWithoutFines,
-                hasPendingFine,
-              };
-            });
-            res.json({ status:true, page, perPage, totalPages, totalItems, data:borrowingsWithFineStatus });
+    const perPage = parseInt(req.query.perPage) || 10;
+    const type = req.query.type || "all";
+    const queryOptions = {
+      offset: (page - 1) * perPage,
+      limit: perPage,
+      include: [
+        {
+          model: Book,
+          as: "book",
+        },
+        {
+          model: Member,
+          as: "member",
+        },
+        {
+          model: Fine,
+          as: "fine",
+        },
+      ],
+    };
+    if (type !== "all") {
+      queryOptions.where = { status: type === "returned" ? false : true };
+    }
+    const totalItems = await Borrowing.count(queryOptions);
+    const totalPages = Math.ceil(totalItems / perPage);
+    const borrowings = await Borrowing.findAll(queryOptions);
+    const borrowingsWithFineStatus = borrowings.map((borrowing) => {
+      const hasPendingFine = borrowing.fine ? !borrowing.fine.isPaid : false;
+      const { borrowings, ...borrowingWithoutFines } = borrowing.toJSON();
+      return {
+        ...borrowingWithoutFines,
+        hasPendingFine,
+      };
+    });
+    res.json({
+      status: true,
+      page,
+      perPage,
+      totalPages,
+      totalItems,
+      data: borrowingsWithFineStatus,
+    });
   } catch (err) {
     res.status(500).json({ status: false, message: err.message });
   }
 };
 const createBorrowing = async (req, res) => {
-    try {
-        console.log(req.body);
+  try {
+    console.log(req.body);
 
-        const book = await Book.findByPk(req.body.bookId, {
-          include: {
-            model: Borrowing,
-            as: "borrowings",
-          },
+    const book = await Book.findByPk(req.body.bookId, {
+      include: {
+        model: Borrowing,
+        as: "borrowings",
+      },
+    });
+    const hasActiveBorrowings = book.borrowings.some(
+      (borrowing) => borrowing.status === true
+    );
+    if (hasActiveBorrowings) {
+      res
+        .status(400)
+        .json({
+          status: false,
+          message: "Book already has an active borrowing",
         });
-        const hasActiveBorrowings = book.borrowings.some(borrowing => borrowing.status === true);
-        if(hasActiveBorrowings){
-            res.status(400).json({status: false, message: 'Book already has an active borrowing'});
-            return;
-        }
-        await Borrowing.create(req.body);
-        res.status(201).json({status: false, message: 'Borrowing created successfully'});
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message});
+      return;
     }
+    await Borrowing.create(req.body);
+    res
+      .status(201)
+      .json({ status: false, message: "Borrowing created successfully" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
-const getBorrowingById =  async (req, res) => {
-    try {
-        const borrowing = await Borrowing.findByPk(req.params.id)
-        res.status(200).json({status : true, data: borrowing})
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message})
-    }
+const getBorrowingById = async (req, res) => {
+  try {
+    const borrowing = await Borrowing.findByPk(req.params.id);
+    res.status(200).json({ status: true, data: borrowing });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
 const deleteBorrowing = async (req, res) => {
-    try {
-        const borrowing = await Borrowing.findByPk(req.params.id,{
-          include: {
-            model: Fine,
-            as: "fine"
-          }
-        });
-        if(borrowing.fine && !borrowing.fine.isPaid){
-            res.status(400).json({status: false, message: 'This borrowing has unpaid fine'});
-        }
-        await borrowing.destroy();
-        res.status(200).json({status: true, message: "Borrowing deleted successfully"});
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message});
+  try {
+    const borrowing = await Borrowing.findByPk(req.params.id, {
+      include: {
+        model: Fine,
+        as: "fine",
+      },
+    });
+    if (borrowing.fine && !borrowing.fine.isPaid) {
+      res
+        .status(400)
+        .json({ status: false, message: "This borrowing has unpaid fine" });
+      return;
     }
+    await borrowing.destroy();
+    res
+      .status(200)
+      .json({ status: true, message: "Borrowing deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
 
 const updateBorrowing = async (req, res) => {
-    try {
-        const borrowing = await Borrowing.findByPk(req.params.id);
-        if(!borrowing){
-            res.status(404).json({status: false, message: "Borrowing not found"});
-            return;
-        }
-        await borrowing.update(req.body);
-        res.status(200).json({status: true, message: "Borrowing updated successfully"});
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message});
+  try {
+    const borrowing = await Borrowing.findByPk(req.params.id);
+    if (!borrowing) {
+      res.status(404).json({ status: false, message: "Borrowing not found" });
+      return;
     }
-}
+    await borrowing.update(req.body);
+    res
+      .status(200)
+      .json({ status: true, message: "Borrowing updated successfully" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
 
-module.exports = {getBorrowings, createBorrowing, getBorrowingById, deleteBorrowing, updateBorrowing}
+module.exports = {
+  getBorrowings,
+  createBorrowing,
+  getBorrowingById,
+  deleteBorrowing,
+  updateBorrowing,
+};
--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,31 +1,38 @@
-const {Member, User, Image, Borrowing, Book} = require('../models');
-const { saveFile } = require('../utils/manageFiles');
+const { Member, User, Image, Borrowing, Book, Fine } = require("../models");
+const { saveFile } = require("../utils/manageFiles");
 
 const getMembers = async (req, res) => {
-    try{
-        const page = parseInt(req.query.page) || 1; 
-          const perPage = parseInt(req.query.perPage) || 10;
-            const totalItems = await Member.count(); 
-            const totalPages = Math.ceil(totalItems / perPage); 
-            const members = await Member.findAll({
-              offset: (page - 1) * perPage,
-              limit: perPage,
-              include: [
-                {
-                model: User,
-                as: "user"
-                },
-              {
-                model: Image,
-                as: 'image'
-              }
-            ]
-            }); 
-            res.json({ status:true, page, perPage, totalPages, totalItems, data:members });
-    } catch(err){
-        res.status(500).json({status: false, message: err.message});
-    }
-}
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
+    const totalItems = await Member.count();
+    const totalPages = Math.ceil(totalItems / perPage);
+    const members = await Member.findAll({
+      offset: (page - 1) * perPage,
+      limit: perPage,
+      include: [
+        {
+          model: User,
+          as: "user",
+        },
+        {
+          model: Image,
+          as: "image",
+        },
+      ],
+    });
+    res.json({
+      status: true,
+      page,
+      perPage,
+      totalPages,
+      totalItems,
+      data: members,
+    });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
 
 const createMember = async (req, res) => {
   try {
@@ -48,15 +55,17 @@ const createMember = async (req, res) => {
         },
       }
     );
-    if(file){
+    if (file) {
       const originalname = file.originalname;
-      const savedFile = await saveFile(file.path, originalname, 'members');
+      const savedFile = await saveFile(file.path, originalname, "members");
       const image = await Image.create(savedFile);
       await user.member.reload();
       await user.member.setImage(image);
     }
 
-    res.status(200).json({ status: true, message: "Member created successfully" });
+    res
+      .status(200)
+      .json({ status: true, message: "Member created successfully" });
   } catch (err) {
     res.status(500).json({ status: false, message: err.message });
   }
@@ -64,76 +73,134 @@ const createMember = async (req, res) => {
 
 const getMemberById = async (req, res) => {
   try {
-    console.log("query member");
-    console.log(req.query.borrowings);
-
     const borrowingPage = parseInt(req.query.borrowingPage) || 1;
-    const borrowingsPerPage = parseInt(req.query.borrowingsPerPage) || 5; // Default value or set as needed
-    const includeBorrowings = req.query.borrowings ? [
-      {
-        model: Borrowing,
-        as: 'borrowings',
-        limit: borrowingsPerPage,
-        offset: (borrowingPage - 1) * borrowingsPerPage,
-        order: [['createdAt', 'DESC']],
-        required: false,          
-        include: {
-          model: Book,
-          as: 'book',
-          required: false          
-        }
-      }
-    ] : [];
+    const borrowingsPerPage = parseInt(req.query.borrowingsPerPage) || 5;
+
+    const includeBorrowings = req.query.borrowings
+      ? [
+          {
+            model: Borrowing,
+            as: "borrowings",
+            limit: borrowingsPerPage,
+            offset: (borrowingPage - 1) * borrowingsPerPage,
+            order: [["createdAt", "DESC"]],
+            required: false,
+            include: [
+              {
+                model: Book,
+                as: "book",
+                required: false,
+              },
+              {
+                model: Fine,
+                as: "fine",
+              },
+            ],
+          },
+        ]
+      : [];
 
     console.log(includeBorrowings);
-       
-    const member = await Member.findByPk(req.params.id, { include: includeBorrowings });
 
-    if(req.query.borrowings){
-      const totalItems = await Borrowing.count({ where: { memberId: req.params.id } });
-      const totalPages = Math.ceil(totalItems / borrowingsPerPage); 
-      res.status(200).json({ status: true, data: member, borrowingPage, borrowingsPerPage, totalItems, totalPages });
+    const member = await Member.findByPk(req.params.id, {
+      include: includeBorrowings,
+    });
+
+    if (req.query.borrowings) {
+      if (!member) {
+        res.status(404).json({ status: false, message: "Member not found" });
+        return;
+      }
+
+      const totalItems = await Borrowing.count({
+        where: { memberId: req.params.id },
+      });
+      const totalPages = Math.ceil(totalItems / borrowingsPerPage);
+
+      // Convert Sequelize instance to plain object to avoid any side effects
+      const memberData = member.toJSON();
+
+      const borrowingsWithFineStatus = memberData.borrowings.map(
+        (borrowing) => {
+          const hasPendingFine = borrowing.fine
+            ? !borrowing.fine.isPaid
+            : false;
+          const { fine, ...borrowingWithoutFines } = borrowing;
+          return {
+            ...borrowingWithoutFines,
+            hasPendingFine,
+          };
+        }
+      );
+
+      memberData.borrowings = borrowingsWithFineStatus;
+
+      res.status(200).json({
+        status: true,
+        data: memberData,
+        borrowingPage,
+        borrowingsPerPage,
+        totalItems,
+        totalPages,
+      });
       return;
     }
+
+    if (!member) {
+      res.status(404).json({ status: false, message: "Member not found" });
+      return;
+    }
+
     res.status(200).json({ status: true, data: member });
   } catch (err) {
     res.status(500).json({ status: false, message: err.message });
   }
 };
+
 const deleteMember = async (req, res) => {
-    try {
-        const member = await Member.findByPk(req.params.id);
-        await member.destroy();
-        res.status(200).json({status: true, message: "Member deleted successfully"});
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message});
-    }
+  try {
+    const member = await Member.findByPk(req.params.id);
+    await member.destroy();
+    res
+      .status(200)
+      .json({ status: true, message: "Member deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
 };
 const updateMember = async (req, res) => {
-    try {
-        const member = await Member.findByPk(req.params.id, {
-          include: {
-            model: Image,
-            as: "image",
-          },
-        });
-        if(!member){ 
-            res.status(404).json({status: false, message: "Member not found"});
-            return;
-        }
-        await member.update(req.body);
-        if(req.file) {
-          const originalname = req.file.originalname;
-          const savedFile = await saveFile(req.file.path, originalname,'members');
-          if(member.image){
-            await member.image.destroy();
-          }
-          const image = await Image.create(savedFile);
-          await member.setImage(image);
-        }
-        res.status(200).json({status: true, message: "Member updated successfully"});
-    } catch (err) {
-        res.status(500).json({status: false, message: err.message});
+  try {
+    const member = await Member.findByPk(req.params.id, {
+      include: {
+        model: Image,
+        as: "image",
+      },
+    });
+    if (!member) {
+      res.status(404).json({ status: false, message: "Member not found" });
+      return;
     }
-}
-module.exports = {getMembers, createMember, getMemberById, deleteMember, updateMember}
+    await member.update(req.body);
+    if (req.file) {
+      const originalname = req.file.originalname;
+      const savedFile = await saveFile(req.file.path, originalname, "members");
+      if (member.image) {
+        await member.image.destroy();
+      }
+      const image = await Image.create(savedFile);
+      await member.setImage(image);
+    }
+    res
+      .status(200)
+      .json({ status: true, message: "Member updated successfully" });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
+module.exports = {
+  getMembers,
+  createMember,
+  getMemberById,
+  deleteMember,
+  updateMember,
+};
